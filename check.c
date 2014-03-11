e068b561d40630515a2e220df5fc1dd486b919cf
Rob Hoelz <rob@hoelz.ro>
2014-03-10 22:57:21 -0500

    Add more thorough checks for Lua 5.1

--- check.c
+++ check.c
@@ -2,6 +2,11 @@
 #include <lualib.h>
 #include <lauxlib.h>
 
+#if LUA_VERSION_NUM != 501
+# error "Lua 5.1 required"
+#endif
+
 int main (void) {
+    void *p = (void *) lua_getfenv;
     return 0;
 }
